bfbb4da5dd7e7d097ed3fb06ee8be1c6d7c51cb2
Vinicius Mussato <vmussato.vermont@vstsalelo.onmicrosoft.com>
2020-12-12 18:46:56 -0300

    body parser and post routes with json

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,10 @@ module.exports = app => {
   app.get('/atendimentos', (req, res) => {
     res.send('você está na rota de atendimentos realizando um get')
   });
+
+  app.post('/atendimentos', (req, res) => {
+    console.log(req.body)
+
+    res.send('Você está na rota de atendimento e está realizando um POST')
+  })
 }
